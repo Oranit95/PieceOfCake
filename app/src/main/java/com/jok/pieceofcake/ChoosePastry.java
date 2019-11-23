3241144f238ef4eb01ed8b46f235f786eab6d1f3
unknown <yardeng321@gmail.com>
2019-11-23 20:22:55 +0200

    yarden

---
+++ app/src/main/java/com/jok/pieceofcake/ChoosePastry.java
@@ -0,0 +1,14 @@
+package com.jok.pieceofcake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoosePastry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_pastry);
+    }
+}
