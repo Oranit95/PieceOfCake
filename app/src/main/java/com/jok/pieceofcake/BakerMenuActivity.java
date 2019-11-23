696b1c19219d89666b99dc2b26d921373683eef5
oranit cohen <oranit95@gmail.com>
2019-11-23 20:24:00 +0200

    test

--- app/src/main/java/com/jok/pieceofcake/BakerMenuActivity.java
+++ app/src/main/java/com/jok/pieceofcake/BakerMenuActivity.java
@@ -6,11 +6,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import java.util.ArrayList;
-import android.app.Activity;
-import android.os.Bundle;
+
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class BakerMenuActivity extends AppCompatActivity {
@@ -18,7 +16,7 @@ public class BakerMenuActivity extends AppCompatActivity {
 
     public class MainActivity extends Activity {
 
-        private List<PastryItem> albumList;
+        private List<PastryItem> pastryList;
         private ListView listView;
         private ListViewAdapter adapter;
         @Override
@@ -27,24 +25,24 @@ public class BakerMenuActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main);
 
             //Create album data list
-            albumList=new ArrayList<>();
-            albumList.add(new PastryItem("עוגת גבינה","120 שקלים"));
-            albumList.add(new PastryItem("עוגת שוקולד","120 שקלים"));
-            albumList.add(new PastryItem("עוגיות שוקולד צ'יפס","120 שקלים"));
-            albumList.add(new PastryItem("עוגת תפוחים","120 שקלים"));
-            albumList.add(new PastryItem("עוגת טורט","120 שקלים"));
-            albumList.add(new PastryItem("עוגת יום הולדת","20 שקלים"));
-            albumList.add(new PastryItem("עוגת תפוזים","40 שקלים"));
-            albumList.add(new PastryItem("עוגת גבינה טבעונית","400 שקלים"));
-            albumList.add(new PastryItem("עוגת קרמשניט","200 שקלים"));
-            albumList.add(new PastryItem("עוגיות שושנים","100 שקלים"));
-            albumList.add(new PastryItem("עוגיות תמרים","90 שקלים"));
-            albumList.add(new PastryItem("כדורי שוקולד","80 שקלים"));
+            pastryList =new ArrayList<>();
+            pastryList.add(new PastryItem("עוגת גבינה","120 שקלים"));
+            pastryList.add(new PastryItem("עוגת שוקולד","120 שקלים"));
+            pastryList.add(new PastryItem("עוגיות שוקולד צ'יפס","120 שקלים"));
+            pastryList.add(new PastryItem("עוגת תפוחים","120 שקלים"));
+            pastryList.add(new PastryItem("עוגת טורט","120 שקלים"));
+            pastryList.add(new PastryItem("עוגת יום הולדת","20 שקלים"));
+            pastryList.add(new PastryItem("עוגת תפוזים","40 שקלים"));
+            pastryList.add(new PastryItem("עוגת גבינה טבעונית","400 שקלים"));
+            pastryList.add(new PastryItem("עוגת קרמשניט","200 שקלים"));
+            pastryList.add(new PastryItem("עוגיות שושנים","100 שקלים"));
+            pastryList.add(new PastryItem("עוגיות תמרים","90 שקלים"));
+            pastryList.add(new PastryItem("כדורי שוקולד","80 שקלים"));
             //Bind listview
             listView=(ListView)findViewById(R.id.listview);
 
             //Create adapter and set it to listview.
-            adapter=new ListViewAdapter(BakerMenuActivity.this,albumList);
+            adapter=new ListViewAdapter(BakerMenuActivity.this, pastryList);
             listView.setAdapter(adapter);
 
         }
--- app/src/main/java/com/jok/pieceofcake/bakerScreen.java
+++ app/src/main/java/com/jok/pieceofcake/bakerScreen.java
@@ -4,15 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 
-import java.util.ArrayList;
 
 public class bakerScreen extends AppCompatActivity {
     ArrayAdapter adapter;
