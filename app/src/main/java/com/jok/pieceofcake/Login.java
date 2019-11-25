c6662d3e4c50613baaeb45178086a9130188f54a
KarinAharon <Krek250@gmail.com>
2019-11-25 16:27:03 +0200

    karin's first

---
+++ app/src/main/java/com/jok/pieceofcake/Login.java
@@ -0,0 +1,25 @@
+package com.jok.pieceofcake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.Confirm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,Choose.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/jok/pieceofcake/MainActivity.java
+++ app/src/main/java/com/jok/pieceofcake/MainActivity.java
@@ -8,37 +8,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
 public class MainActivity extends AppCompatActivity {
     Button customer;
     Button baker;
     Typeface font;
     Typeface bakerF;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("test");
-        customer = (Button) findViewById(R.id.customer);
-        baker = (Button) findViewById(R.id.baker);
+        customer = (Button) findViewById(R.id.Customer);
+        baker = (Button) findViewById(R.id.Baker);
         font = Typeface.createFromAsset(this.getAssets(), "fonts/Anka CLM Bold.ttf");
         customer.setTypeface(font);
         baker.setTypeface(font);
+        customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
         baker.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            moveToActivitySchedule();
-                                        }
-                                    }
-        );
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,bakerScreen.class);
+                startActivity(intent);
+            }
+        });
 
     }
-
-    private void moveToActivitySchedule() {
-        Intent intent = new Intent(this, bakerScreen.class);
-        startActivity(intent);
-    }
-
 }
-
