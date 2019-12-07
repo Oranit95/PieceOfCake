cc7d81409e266dd241972f9a16fa8eb4c065cec8
oranit cohen <oranit95@gmail.com>
2019-12-07 14:40:14 +0200

    commit

--- app/src/main/java/com/jok/pieceofcake/BakerMenu.java
+++ app/src/main/java/com/jok/pieceofcake/BakerMenu.java
@@ -1,8 +1,12 @@
 package com.jok.pieceofcake;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -11,26 +15,56 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BakerMenu extends AppCompatActivity {
+    public static final String TAG = "TAG_ADD_PASTRY";
     String userID;
     private FirebaseAuth FireLog;// fire base authentication
     FirebaseFirestore fStore; //firebase DB
+    EditText price, name, description, allergenic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baker_menu2);
         FireLog = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
+        retrieve();
     }
 
     public void addItem(View view) {
         FirebaseUser user = FireLog.getCurrentUser();
         userID = user.getUid();
+        String priceIn = price.getText().toString().trim();
+        String nameIn = name.getText().toString().trim();
+        String descIn = description.getText().toString().trim();
+        String allergicIn = allergenic.getText().toString().trim();
+
         DocumentReference docBaker = fStore.collection("Bakers").document(userID).collection("Menu")
-                .document("pastry name - **need to change***");
+                .document(nameIn);
         Map<String, Object> pastryDetails = new HashMap<>();
-        pastryDetails.put("Full Name","" );
+        pastryDetails.put("מחיר",priceIn );
+        pastryDetails.put("תיאור",descIn );
+        pastryDetails.put("רכיבים אלרגניים",allergicIn );
+        docBaker.set(pastryDetails).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "onSuccess: added pastry "+ userID);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailue" + e.toString());
+            }
+        });
+    }
+
+    public void retrieve() {
+        price = (EditText) findViewById(R.id.priceInput);
+        name = (EditText) findViewById(R.id.nameInput);
+        description = (EditText) findViewById(R.id.desInput);
+        allergenic = (EditText) findViewById(R.id.alerganicInput);
     }
 }
