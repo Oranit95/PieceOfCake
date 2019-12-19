253cc4815852686e1af118b79993b7833678db7d
KarinAharon <Krek250@gmail.com>
2019-12-19 19:46:03 +0200

    update activity buy pastry

--- app/src/main/java/com/jok/pieceofcake/customerSide/BuyPastryPopUpActivity.java
+++ app/src/main/java/com/jok/pieceofcake/customerSide/BuyPastryPopUpActivity.java
@@ -14,12 +14,12 @@ public class BuyPastryPopUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_pastry_pop_up);
-
+/**
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
-        getWindow().setLayout((int)(dm.widthPixels*.8), (int)(dm.heightPixels*.6));
-
+        getWindow().setLayout((int)(dm.widthPixels*.8), (int)(dm.heightPixels*.8));
+**/
     }
 
     public void sendOrderToBaker(View view) {
