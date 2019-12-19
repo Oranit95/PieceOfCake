fc2720ff9cea738c488a03131ae5815293298665
oranit cohen <oranit95@gmail.com>
2019-12-19 16:12:26 +0200

    added bakers list to customer side, need to check it

--- app/src/main/java/com/jok/pieceofcake/customerSide/BakerAdapter.java
+++ app/src/main/java/com/jok/pieceofcake/customerSide/BakerAdapter.java
@@ -1,4 +1,51 @@
 package com.jok.pieceofcake.customerSide;
 
-public class BakerAdapter {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.jok.pieceofcake.R;
+import com.jok.pieceofcake.bakerSide.Baker;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class BakerAdapter extends ArrayAdapter<Baker> {
+    private Activity context;
+    private List<Baker> bakers;
+
+    public BakerAdapter(Activity context, List<Baker> bakers ){
+        super(context, R.layout.baker_item,bakers);
+        this.context = context;
+        this.bakers = bakers;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater =  context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.baker_item, null, true);
+
+        TextView address = listViewItem.findViewById(R.id.BakerAddress);
+        TextView name = listViewItem.findViewById(R.id.BakerName);
+
+
+        Baker baker = bakers.get(position);
+        address.setText("כתובת: "+baker.getAddress());
+        name.setText("שם: "+baker.getFull_name());
+
+        return listViewItem;
+    }
+
+    @Override
+    public int getCount() {
+        if(bakers == null) return 0;
+        else return bakers.size();
+    }
 }
--- app/src/main/java/com/jok/pieceofcake/customerSide/SearchPastry.java
+++ app/src/main/java/com/jok/pieceofcake/customerSide/SearchPastry.java
@@ -1,16 +1,84 @@
 package com.jok.pieceofcake.customerSide;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.jok.pieceofcake.R;
+import com.jok.pieceofcake.bakerSide.Baker;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchPastry extends AppCompatActivity {
+    private FirebaseAuth FireLog;// fire base authentication
+    ListView listViewBakers;
+    String userID;
+    DatabaseReference bakersRef;
+    FirebaseDatabase DB;
+    FirebaseUser currentUser;
+    List<Baker>  bakerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_pastry);
+        DB = FirebaseDatabase.getInstance();
+        FireLog = FirebaseAuth.getInstance();
+        currentUser = FireLog.getCurrentUser();
+        userID = currentUser.getUid();
+        listViewBakers = findViewById(R.id.bakersListView);
+
+        bakerList = new ArrayList<Baker>();
+
+        bakersRef = DB.getReference("Users").child("Bakers");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        userID = FireLog.getCurrentUser().getUid();
+        bakersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                bakerList.clear();
+                for (DataSnapshot bakersnapShot : dataSnapshot.getChildren()) {
+                    Baker baker = bakersnapShot.getValue(Baker.class);
+                    bakerList.add(baker);
+                }
+                if (bakerList.isEmpty()) {
+                    Toast.makeText(SearchPastry.this, "התפריט ריק! הוסף מאפה חדש", Toast.LENGTH_LONG).show();
+                   // moveToAddPastry();
+                    return;
+                }
+                BakerAdapter bakerAdapter = new BakerAdapter(SearchPastry.this, bakerList);
+                listViewBakers.setAdapter(bakerAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        listViewBakers.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+        });
+
     }
 }
