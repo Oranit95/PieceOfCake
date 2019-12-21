645814e37f6c0f002e45cce5b737877787db8556
KarinAharon <Krek250@gmail.com>
2019-12-21 19:08:50 +0200

    save baker and customer order in database

--- app/src/main/java/com/jok/pieceofcake/customerSide/OrderAdapter.java
+++ app/src/main/java/com/jok/pieceofcake/customerSide/OrderAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.jok.pieceofcake.Order;
 import com.jok.pieceofcake.R;
@@ -30,22 +31,61 @@ public class OrderAdapter extends ArrayAdapter<Order> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View listViewItem = inflater.inflate(R.layout.order_item, null, true);
-/**
-        TextView numOfOrder = listViewItem.findViewById(R.id.numOforderC);
-        TextView B_email = listViewItem.findViewById(R.id.baker_email);
-        TextView City = listViewItem.findViewById(R.id.city_C);
-        TextView date = listViewItem.findViewById(R.id.date_C);
-        TextView pastry_name = listViewItem.findViewById(R.id.pastry_name_C);
-        TextView pastry_id = listViewItem.findViewById(R.id.pastry_id_C);
-        TextView pay = listViewItem.findViewById(R.id.pay_C);
+        TextView B_phone = listViewItem.findViewById(R.id.bakerPhone);
+        TextView C_phone = listViewItem.findViewById(R.id.customerPhone);
+        TextView B_name = listViewItem.findViewById(R.id.bakerName);
+        TextView C_name = listViewItem.findViewById(R.id.customerName);
+        TextView B_email = listViewItem.findViewById(R.id.bakerEmail);
+        TextView C_email = listViewItem.findViewById(R.id.customerEmail);
+        TextView B_City = listViewItem.findViewById(R.id.bakerCity);
+        TextView C_City = listViewItem.findViewById(R.id.customerCity);
+        TextView B_Street = listViewItem.findViewById(R.id.bakerStreet);
+        TextView C_Street = listViewItem.findViewById(R.id.customerStreet);
+        TextView B_House = listViewItem.findViewById(R.id.bakerNumHouse);
+        TextView C_House = listViewItem.findViewById(R.id.customerNumHouse);
+        TextView B_floor = listViewItem.findViewById(R.id.bakerFloor);
+        TextView C_floor = listViewItem.findViewById(R.id.customerFloor);
+        TextView B_appartment = listViewItem.findViewById(R.id.bakerAppartment);
+        TextView C_appartment = listViewItem.findViewById(R.id.customerAppartment);
+        TextView date = listViewItem.findViewById(R.id.dateOrder);
+        TextView pay = listViewItem.findViewById(R.id.PayOrder);
+        TextView recievedOrder = listViewItem.findViewById(R.id.recievedOrder);
+        TextView namePastry = listViewItem.findViewById(R.id.namePastry);
+        TextView comments = listViewItem.findViewById(R.id.Comment);
 
         Order order = ordersList.get(position);
-        numOfOrder.setText("מספר הזמנה: " +order.getNumOfOrder());
-        B_email.setText("מייל האופה: " +order.getB_email());
-        //City.setText("עיר: " +order.getCity());
+        B_email.setText("מייל האופה: " +order.getBaker().getEmail());
+        C_email.setText("מייל הלקוח: " +order.getCustomer().getEmail());
+        B_phone.setText("טלפון האופה: " +order.getBaker().getPhone());
+        C_phone.setText("טלפון הלקוח: " +order.getCustomer().getPhone());
+        B_name.setText("שם האופה: " +order.getBaker().getFull_name());
+        C_name.setText("שם הלקוח: " +order.getCustomer().getFull_name());
+        B_City.setText("עיר: " +order.getBaker().getAddress().getCity());
+        C_City.setText("עיר: " +order.getCustomer().getAddress().getCity());
+        B_Street.setText("רחוב: " +order.getBaker().getAddress().getStreetName());
+        C_Street.setText("רחוב: " +order.getCustomer().getAddress().getStreetName());
+        B_House.setText("מספר בית: " +order.getBaker().getAddress().getBuildingNumber());
+        C_House.setText("מספר בית: " +order.getCustomer().getAddress().getBuildingNumber());
+        B_floor.setText("קומה: " +order.getBaker().getAddress().getFloor());
+        C_floor.setText("קומה: " +order.getCustomer().getAddress().getFloor());
+        B_appartment.setText("דירה: " +order.getBaker().getAddress().getAppartmentNumber());
+        C_appartment.setText("דירה: " +order.getCustomer().getAddress().getAppartmentNumber());
         date.setText("תאריך: " +order.getDate());
-        pastry_name.setText("שם המאפה: " +order.getPastry_name());
- pastry_id.setText("מזהה המאפה: " +order.getPastry_id());**/
+        namePastry.setText("שם המאפה: " +order.getPastry_name());
+        comments.setText("הערות: " +order.getComments());
+        if(order.isCard() == true){
+            pay.setText("אמצעי תשלום: אשראי");
+        }
+        else {
+            pay.setText("אמצעי תשלום: מזומן");
+        }
+
+        if (order.isDelivery()==true){
+            recievedOrder.setText("קבלת ההזמנה: משלוח");
+        }
+        else{
+            recievedOrder.setText("קבלת ההזמנה: איסוף עצמי");
+        }
         return listViewItem;
     }
 }
